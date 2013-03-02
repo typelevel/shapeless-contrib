cf3cdcc69ffe6ac7844932283fc3d267c79e10fe
Lars Hupel <lars.hupel@mytum.de>
2013-03-02 18:57:00 +0100

    simplify package structure
    First of all, each submodules contained both an object and a package object
    with the same members. For now, only the package object is used.
    
    This change was prompted because the redundant objects were called
    `typeClass`, and since there were also traits called `TypeClass`, this caused
    massive confusion under case-insensitive file systems.

--- scalacheck/main/scala/package.scala
+++ scalacheck/main/scala/package.scala
@@ -1,5 +1,30 @@
 package shapeless.contrib
 
-package object scalacheck extends TypeClasses
+import shapeless._
+
+import scalaz._
+import scalaz.syntax.apply._
+import scalaz.scalacheck.ScalaCheckBinding._
+
+import org.scalacheck.{Gen, Arbitrary}
+
+package object scalacheck {
+
+  implicit def ArbitraryI: TypeClass[Arbitrary] = new TypeClass[Arbitrary] {
+
+    def emptyProduct = Arbitrary(Gen.value(HNil))
+
+    def product[F, T <: HList](f: Arbitrary[F], t: Arbitrary[T]) =
+      (f |@| t) { _ :: _ }
+
+    def derive[A, B](b: Arbitrary[B], ab: Iso[A, B]) =
+      b.map(ab.from)
+
+  }
+
+  implicit def deriveArbitrary[F, G <: HList](implicit iso: Iso[F, G], hlistInst: TypeClass.HListInstance[Arbitrary, G]): Arbitrary[F] =
+    TypeClass.deriveFromIso[Arbitrary, F, G]
+
+}
 
 // vim: expandtab:ts=2:sw=2
